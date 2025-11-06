83dc8b3e600d210d015d43180bbc313022d3f142
KobeSmitsPXL <Kobe.smits@student.pxl.be>
2025-11-06 10:13:35 +0100

    oefeningen

---
+++ LaboClasses/Models/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboClasses.Models
+{
+    internal class Employee
+    {
+        
+		private string _firstName;
+
+		public string FirstName
+		{
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
+		
+		private string  _lastName;
+
+		public string  LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+		private DateTime _birthDay;
+
+		public  DateTime BirthDay
+		{
+			get { return _birthDay; }
+			set { _birthDay = value; }
+		}
+		private double _salary;
+
+		public double Salary
+		{
+			get { return _salary; }
+			set { _salary = value; }
+		}
+        public int Age	 
+		{ 
+			get 
+			{
+				DateTime now = DateTime.Now;
+				TimeSpan yearsAlive = now - BirthDay;
+                return (int) (yearsAlive.TotalDays / 365);
+			}
+		}
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+		public void IncreaseSalary(int percentage)
+		{
+			Salary += Salary * percentage / 100;
+        }
+
+		//Overrrid = overschrijft het standaard gedrag 
+		// ALT + ENTER -> Generate overrides . . .
+		//public override string? ToString()
+		//{
+		//    return base.ToString();
+		//}
+		public override string ToString()
+		{
+			return $"{FirstName} {LastName} - {Age} - {Salary}";
+		}
+	}
+}
---
+++ LaboClasses/Program.cs
@@ -0,0 +1,63 @@
+﻿using LaboClasses.Models;
+using System.Text;
+
+namespace LaboClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Voornaam: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Achternaam: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Geboortedatum: ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Salaris: ");
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            //Voorbeeld voor constructor zonder parameters
+            Employee me = new Employee();
+            me.FirstName = firstName;
+            me.LastName = lastName;
+            me.BirthDay = birthDate;
+            me.Salary = (double)salary;
+
+            //Voorbeeld voor constructor met parameters
+            me = new Employee(firstName, lastName);
+            me.BirthDay = birthDate;
+            me.Salary = (double)salary;
+
+            Console.WriteLine(me.ToString());
+            Console.WriteLine(ShowDetails(me));
+
+            int percentage;
+            Console.WriteLine("Geef een percentage in van nul tot tien opslag");
+            while (!(int.TryParse(Console.ReadLine(), out percentage) && (percentage >= 0 && percentage <= 10)))
+            {
+                Console.WriteLine("Ongeldige input");
+            }
+            me.IncreaseSalary(percentage);
+            
+            Console.WriteLine (ShowDetails(me));
+
+
+
+        }
+        private static string ShowDetails(Employee me)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"----------------------------------------------");
+            sb.AppendLine($"Werknemer:{me.FirstName} {me.LastName}");
+            sb.AppendLine($"Geboortedatum: {me.BirthDay.ToLongDateString()} ({me.Age})");
+            sb.AppendLine($"Salaris: {me.Salary}");
+            sb.AppendLine($"----------------------------------------------");
+            sb.AppendLine($"Samenvatting: {me.FirstName} {me.LastName} - {me.Age} - {me.Salary}");
+            sb.AppendLine($"--------------------------------------------------");
+            return sb.ToString();
+        }
+    }
+}
